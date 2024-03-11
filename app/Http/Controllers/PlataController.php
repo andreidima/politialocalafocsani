1b09e82a9042b68af3ff70479e6f52c7cf6c3def
Andrei <andrei.dima@usm.ro>
2024-03-11 14:50:32 +0100

    a

--- app/Http/Controllers/PlataController.php
+++ app/Http/Controllers/PlataController.php
@@ -366,11 +366,11 @@ class PlataController extends Controller
     public function verificarePlataNumarInmatriculare(Request $request)
     {
         $plati = null;
-        if ($nr_inmatriculare = $request->searchNumarInmatriculare){
-            $nr_inmatriculare = preg_replace("/[^a-zA-Z0-9]+/", "", $nr_inmatriculare);
-            $plati = Plata::where('nr_inmatriculare', $nr_inmatriculare)->whereDate('data_sfarsit', '>=', Carbon::today())->get();
+        if ($searchNumarInmatriculare = $request->searchNumarInmatriculare){
+            $searchNumarInmatriculare = preg_replace("/[^a-zA-Z0-9]+/", "", $searchNumarInmatriculare);
+            $plati = Plata::where('nr_inmatriculare', $searchNumarInmatriculare)->whereDate('data_sfarsit', '>=', Carbon::today())->get();
         }
 
-        return view('plati.verificarePlataNumarInmatriculare', compact('plati'));
+        return view('plati.verificarePlataNumarInmatriculare', compact('plati', 'searchNumarInmatriculare'));
     }
 }
